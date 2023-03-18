3dd3016924ab31616ec20066bb6b74a69c3aafbf
Melvin Mathew <vinumon2000@gmail.com>
2023-03-18 11:30:11 +0530

    clone created

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,11 +1,61 @@
-import Header from "./components/Header";
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
+import Movie from './components/MovieRow';
+import './App.css';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
 
-function App() {
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
+
+    }
+
+    loadAll();
+  }, []);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
+
   return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
-}
+    <div className='page'>
+      <Header black={blackHeader} />
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />}
+      <section className='lists'>
+        {movieList.map((item, key) => (<Movie key={key} title={item.title} items={item.items} />))}
+      </section>
 
-export default App;
+
+      {movieList.length <= 0 &&
+        <div className='loading'>
+          <img src='https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif' alt='Carregando' size={13}></img>
+        </div>
+      }
+    </div>
+  )
+}

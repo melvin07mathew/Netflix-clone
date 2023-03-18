0811214ba11fb88f7d08b165adddbeb3e919eb30
Melvin Mathew <vinumon2000@gmail.com>
2023-03-18 10:46:29 +0530

    some changes in header

--- netflix/src/components/Header.js
+++ netflix/src/components/Header.js
@@ -1,9 +1,9 @@
 import React from "react"
 import  "./Header.css"
 
-export default ({black}) => {
+export default () => {
     return (
-        <header className={black ? 'black' : ''}>
+        <header>
         <div className="header--logo">
             <a href="/">
                 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/200px-Netflix_2015_logo.svg.png"></img>
